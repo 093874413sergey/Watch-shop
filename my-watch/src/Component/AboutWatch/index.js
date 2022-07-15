b30dfd79036033afa92892f4b725eed503e87242
kvark85 <IlKaN1985>
2022-07-15 14:48:18 +0300

    add routing

---
+++ my-watch/src/Component/AboutWatch/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function AboutWatch() {
+  return (
+    <>
+      <h1>Замечательные часы</h1>
+
+      <p>Эти замечетальные часы бла бла бла...</p>
+
+      <a href="/">Вернуться назад</a>
+    </>
+  )
+}
+
+
+export default AboutWatch;
