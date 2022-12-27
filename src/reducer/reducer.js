85e636c6c76ce1c2d07e0723b3d5d887beb813ec
Andrii_Bespalyi <kvark1985@gmail.com>
2022-12-27 16:36:06 +0200

    improve your js skills

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -2,60 +2,25 @@ const initialState = {
   basketItems:[]
 };
 
+export const yourAddFunction = (basketItems, itemToAdd) => {
+  return [] // вместо "return []" напиши свою функцию которая будет добавлять товар
+}
+
+export const yourDeleteFunction = (basketItems, idToDelete) => {
+  return [] // вместо "return []" напиши свою функцию которая будет уменьшьать или удалять товары
+}
+
 export function basketReducer(state = initialState, action) {
   switch (action.type) {
     case 'ADD_ITEM':
-      const itemIds = state.basketItems.map(({itemData}) => itemData.id)
-      const isItemExist = itemIds.includes(action.payload.id)
-
-      if(isItemExist) {
-        return {
-          ...state,
-          basketItems: [
-            ...state.basketItems.map(item => {
-              if(item.itemData.id === action.payload.id) {
-                return {
-                  quantity: item.quantity + 1,
-                  itemData: item.itemData,
-                }
-              }
-
-              return item
-            }),
-          ]
-        }
-      }
-
       return {
         ...state,
-        basketItems: [
-          ...state.basketItems,
-          {
-            quantity: 1,
-            itemData: action.payload
-          },
-        ]
+        basketItems: yourAddFunction(state.basketItems, action.payload)
       }
     case 'DELETE_ITEM':
-      let newItems = []
-
-      for(let index=0; index < state.basketItems.length; index++) {
-
-        if(state.basketItems[index].itemData.id !== action.payload.id) {
-          newItems.push(state.basketItems[index])
-        }
-
-        if(state.basketItems[index].quantity >= 2) {
-          newItems.push({
-            quantity: state.basketItems[index].quantity - 1,
-            itemData: action.payload,
-          })
-        }
-      }
-
       return {
         ...state,
-        basketItems: newItems,
+        basketItems: yourDeleteFunction(state.basketItems, action.payload.id),
       }
     default:
       return state
